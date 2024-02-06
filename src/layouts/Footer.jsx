6fcb84d09323cbf1682718defef80b494dc723f5
Vlad <sambipush@mail.ru>
2024-02-06 11:03:14 +0300

    Create Footer component

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,17 +1,20 @@
-// import React from "react";
-// import { logo } from "../assets";
+import React from "react";
+import { logo } from "../assets";
+import { Link } from "react-router-dom";
 
-// const Footer = () => {
-//     return (
-//         <div className="flex fixed bottom-0">
-//             <div>
-//                 <a href="">
-//                     <img src={logo} alt="logo" className="w-[150px] h-[30px]" />
-//                 </a>
-//             </div>
-//             <a href="!#">CodeLink</a>
-//         </div>
-//     );
-// };
+const Footer = () => {
+    return (
+        <div className="flex items-center justify-between mt-10 py-3 relative before:absolute before:top-0 before:left-0 before:w-full before:bg-dark before:h-[1px]">
+            <Link to={"/"}>
+                <img
+                    src={logo}
+                    alt="logo"
+                    className="sfhd:w-[250px] sfhd:h-[60px] w-[150px] h-[30px]"
+                />
+            </Link>
+            <a href="!#">CodeLink</a>
+        </div>
+    );
+};
 
-// export default Footer;
+export default Footer;
