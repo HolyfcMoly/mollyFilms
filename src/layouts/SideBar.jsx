4264ecc44b346b838b060ca7e63f462cbc55bde1
Vlad <sambipush@mail.ru>
2024-02-17 12:00:49 +0300

    create handleGenreChange for set in localStorage genre.

--- src/layouts/SideBar.jsx
+++ src/layouts/SideBar.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
-import { getGenre } from "../services/api";
+import { getGenre, getTvGenre } from "../services/api";
 import { Link } from "react-router-dom";
 import GenrePreLoader from "../components/ui/GenresPreLoader";
 import BurgerBtn from "../components/ui/BurgerBtn";
 import { genreAndCategoriesIcons } from "../assets/icons/genres";
 import useResize from "../hooks/useResize";
-import { clickOutside } from "../utils";
+import { clickOutSide, filterDuplicates } from "../utils";
 
 const SideBar = () => {
     const [data, setData] = useState([]);
@@ -13,6 +13,7 @@ const SideBar = () => {
     const [loading, setLoading] = useState(true);
     const [isMobile, setIsMobile] = useState(false);
     const width = useResize();
+    const [seriesGenre, setSeriesGenre] = useState([]);
 
     const categories = [
         { label: "Популярные", value: "popular" },
@@ -23,9 +24,18 @@ const SideBar = () => {
     const menuRef = useRef(null);
     const skeletonData = new Array(19).fill("id");
 
+    const handleGenreChange = () => {
+        localStorage.setItem("isGenreChanged", true);
+    };
+
     useEffect(() => {
         getGenre().then((data) => {
             data && setData(data);
+            setLoading(false);
+        });
+
+        getTvGenre().then((data) => {
+            data && setSeriesGenre(data);
             setLoading(false);
         });
     }, []);
@@ -34,8 +44,13 @@ const SideBar = () => {
         setIsMobile(false);
         if (width < 1060) {
             setIsMobile(true);
-            const handler = clickOutside(menuRef, setToggle)
-            document.addEventListener("click", handler);
+            const toggleSideBar = () => {
+                !toggle ? setToggle(false) : setToggle(!toggle);
+            };
+            document.addEventListener("click", (e) => {
+                clickOutSide(e, menuRef, toggleSideBar);
+            });
+            return () => document.removeEventListener("click", clickOutSide);
         }
     }, [width, toggle]);
 
@@ -43,7 +58,7 @@ const SideBar = () => {
         <div>
             {isMobile && <BurgerBtn setToggle={setToggle} toggle={toggle} />}
             <div
-                className={`fixed overflow-auto bottom-0 sfhd:top-[7.65rem] ss:top-[6.3rem] md:top-[5.8rem] top-[8.8rem] ${
+                className={`fixed overflow-auto bottom-0 sfhd:top-[5.5rem] top-[3.5rem] ${
                     toggle ? "sm:left-[2rem] left-[1rem]" : "left-[-500px]"
                 } md:left-[unset] transition-[left] ease-in-out duration-500 custom_scroll sfhd:text-3xl sfhd:w-[350px] w-[200px] xl:w-[250px] bg-main z-30`}
                 ref={menuRef}
@@ -58,7 +73,9 @@ const SideBar = () => {
                                 <li
                                     key={value}
                                     className="sfhd:py-6 xl:py-3 p-1 active:text-active hover:text-secondary transition-colors duration-300 focus-within:text-secondary"
-                                    onClick={() => setToggle(!toggle)}
+                                    onClick={() => {
+                                        setToggle(!toggle);
+                                    }}
                                 >
                                     <Link
                                         to={`/`}
@@ -66,11 +83,14 @@ const SideBar = () => {
                                             movies: value,
                                             genre: label,
                                         }}
+                                        onClick={() => handleGenreChange() }
                                         className="block focus:outline-none focus-visible:ring-1 focus-visible:ring-secondary rounded-[5px]"
                                     >
                                         <div className="flex items-center p-2 sfhd:genre-icons-xl ">
                                             {genreAndCategoriesIcons[value]}
-                                            <p className="ml-4 sfhd:text-2xl xl:text-xl">{label}</p>
+                                            <p className="ml-4 sfhd:text-2xl xl:text-xl">
+                                                {label}
+                                            </p>
                                         </div>
                                     </Link>
                                 </li>
@@ -102,6 +122,7 @@ const SideBar = () => {
                                                 movies: item.id,
                                                 genre: item.name,
                                             }}
+                                            onClick={() => handleGenreChange() }
                                             className="block focus:outline-none focus-visible:ring-1 focus-visible:ring-secondary rounded-[5px]"
                                         >
                                             <div className="flex items-center p-2 sfhd:genre-icons-xl genre-icons">
