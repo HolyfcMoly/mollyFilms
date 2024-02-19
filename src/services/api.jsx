c99ec9e7c8fa5089fd928d7836d125698ad7ab0a
Vlad <sambipush@mail.ru>
2024-02-19 11:00:45 +0300

    Create new api methods. And create AbortController api and create refreshAPI() method to prevent multiply requests. some fixes.

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,26 @@
 import { API_KEY, API_URL } from "./config";
 import axios from "axios";
 
+let controller = null;
+
+const createApi = () => {
+    controller = new AbortController();
+    return axios.create({
+        signal: controller.signal,
+    });
+}
+
+let api = createApi();
+
+const cancelRequests = () => {
+    controller.abort();
+};
+
+export const refreshAPI = () => {
+    cancelRequests();
+    api = createApi(); 
+  }
+
 const getGenre = () => {
     const response = axios
         .get(`${API_URL}genre/movie/list?api_key=${API_KEY}&language=ru`)
@@ -9,8 +29,16 @@ const getGenre = () => {
     return response;
 };
 
+const getTvGenre = () => {
+    const response = axios
+        .get(`${API_URL}genre/tv/list?api_key=${API_KEY}&language=ru`)
+        .then((res) => res.data.genres)
+        .catch((e) => console.log(e));
+    return response;
+};
+
 const getMovies = async (category = "popular", page = 1) => {
-    const response = await axios
+    const response = await api
         .get(
             `${API_URL}movie/${category}?api_key=${API_KEY}&page=${page}&language=ru`
         )
@@ -19,13 +47,21 @@ const getMovies = async (category = "popular", page = 1) => {
     return response;
 };
 
-const getMoviesByGenre = async (genreId, page = 1) => {
-    const response = await axios
-        .get(
-            `${API_URL}discover/movie/?with_genres=${genreId}&api_key=${API_KEY}&page=${page}&language=ru`
-        )
+const getPopularTv = async (page = 1) => {
+    const response = await api
+        .get(`${API_URL}tv/popular?api_key=${API_KEY}&page=${page}&language=ru`)
         .then((res) => res)
         .catch((e) => console.log(e));
+    return response;
+};
+
+const getMoviesByGenre = async (genreId, page = 1) => {
+    const response = await api
+        .get(
+            `${API_URL}discover/movie?with_genres=${genreId}&api_key=${API_KEY}&page=${page}&language=ru`
+        )
+        .then((res) => res)
+        .catch((e) => e);
     return response;
 };
 
@@ -33,6 +69,16 @@ const getMovieByQuerySearch = async (query, page) => {
     const response = await axios
         .get(
             `${API_URL}search/movie?query=${query}&api_key=${API_KEY}&page=${page}&language=ru`
+        )
+        .then((res) => res)
+        .catch((e) => console.log(e));
+    return response;
+};
+
+const getMultiSearchByQuery = async (query, page) => {
+    const response = await axios
+        .get(
+            `${API_URL}search/multi?query=${query}&api_key=${API_KEY}&page=${page}&language=ru`
         )
         .then((res) => res)
         .catch((e) => console.log(e));
@@ -47,15 +93,25 @@ const getMovie = async (id) => {
     return response;
 };
 
-const getNowPlaying = async (page = 1) => {
+const getSeries = async (id) => {
     const response = await axios
-        .get(`${API_URL}movie/now_playing?page=${page}&api_key=${API_KEY}&language=ru`)
+        .get(`${API_URL}tv/${id}?api_key=${API_KEY}&language=ru`)
         .then((res) => res)
         .catch((e) => console.log(e));
     return response;
 };
 
-const getRecomendMovies = async (id) => {
+const getNowPlaying = async (page = 1) => {
+    const response = await axios
+        .get(
+            `${API_URL}movie/now_playing?page=${page}&api_key=${API_KEY}&language=ru`
+        )
+        .then((res) => res)
+        .catch((e) => console.log(e));
+    return response;
+};
+
+const getRecommendMovies = async (id) => {
     const response = await axios
         .get(
             `${API_URL}movie/${id}/recommendations?api_key=${API_KEY}&language=ru`
@@ -65,9 +121,27 @@ const getRecomendMovies = async (id) => {
     return response;
 };
 
+const getRecommendSeries = async (id) => {
+    const response = await axios
+        .get(
+            `${API_URL}tv/${id}/recommendations?api_key=${API_KEY}&language=ru`
+        )
+        .then((res) => res)
+        .catch((e) => console.log(e));
+    return response;
+};
+
 const getCredits = async (id) => {
     const response = await axios
         .get(`${API_URL}movie/${id}/credits?api_key=${API_KEY}&language=ru`)
+        .then((res) => res)
+        .catch((e) => console.log(e));
+    return response;
+};
+
+const getSeriesCredits = async (id) => {
+    const response = await axios
+        .get(`${API_URL}tv/${id}/credits?api_key=${API_KEY}&language=ru`)
         .then((res) => res)
         .catch((e) => console.log(e));
     return response;
@@ -107,6 +181,22 @@ const getMovieTrailer = async (id) => {
     return response;
 };
 
+const getSeriesTrailer = async (id) => {
+    const response = await axios
+        .get(`${API_URL}tv/${id}/videos?api_key=${API_KEY}&language=en}`)
+        .then((res) => res)
+        .catch((e) => console.log(e));
+    return response;
+};
+
+const getExternalIds = async (id) => {
+    const response = await axios
+        .get(`${API_URL}tv/${id}/external_ids?api_key=${API_KEY}`)
+        .then((res) => res)
+        .catch((e) => console.log(e));
+    return response;
+};
+
 export {
     getMovie,
     getMovies,
@@ -119,5 +209,16 @@ export {
     getGenre,
     getPosterImage,
     getNowPlaying,
-    getRecomendMovies,
+    getRecommendMovies,
+    getMultiSearchByQuery,
+    //series
+    getTvGenre,
+    getPopularTv,
+    getSeries,
+    getSeriesCredits,
+    getSeriesTrailer,
+    getRecommendSeries,
+    getExternalIds,
+    //utils
+    cancelRequests,
 };
