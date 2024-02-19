ca8ac105390ea33162c06005c138b7675293fba3
Vlad <sambipush@mail.ru>
2024-02-19 11:02:11 +0300

    Add api methods for Tv series and some information about. Create movieStatus to display status for the movies and Tv series, some fixes and updates.

--- src/pages/MovieInformation.jsx
+++ src/pages/MovieInformation.jsx
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from "react";
 import {
     getCredits,
+    getExternalIds,
     getMovie,
     getMovieTrailer,
-    getRecomendMovies,
+    getRecommendMovies,
+    getRecommendSeries,
+    getSeries,
+    getSeriesCredits,
+    getSeriesTrailer,
 } from "../services/api";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import MovieList from "../components/MovieList";
 import BackButton from "../components/ui/BackButton";
 import ProfileSlider from "../components/ProfileSlider";
 import Poster from "../components/ui/Poster";
 import { Rating, ThinStar } from "@smastrom/react-rating";
-import { convertRuntime, filteredJob } from "../utils";
+import {
+    convertRuntime,
+    filterDuplicates,
+    filteredJob,
+    getStringDeclination,
+} from "../utils";
 import { genreAndCategoriesIcons } from "../assets/icons/genres";
 import ImdbBtn from "../components/ui/ImdbBtn";
 import TrailerBtn from "../components/ui/TrailerBtn";
@@ -25,41 +35,79 @@ const myStyles = {
 
 const MovieInformation = () => {
     const { id } = useParams();
+    const location = useLocation();
     const [movie, setMovie] = useState({});
     const [movies, setMovies] = useState([]);
     const [credits, setCredits] = useState([]);
     const [crew, setCrew] = useState([]);
     const [video, setVideo] = useState([]);
     const [trailerVideo, setTrailerVideo] = useState([]);
+    const [externalIds, setExternalIds] = useState({});
+
     const genres = movie.genres || [];
     const runtime = convertRuntime(movie.runtime);
+    const seasonCount = getStringDeclination(movie.number_of_seasons, "сезон");
 
-    const director = filteredJob(crew, "Directing");
-    const producer = filteredJob(crew, "Production");
-    const operator = filteredJob(crew, "Camera");
-    const writer = filteredJob(crew, "Writing");
+    const director = filteredJob(crew, "Directing", "department");
+    const producer = filteredJob(crew, "Production", "department");
+    const operator = filteredJob(crew, "Camera", "department");
+    const writer = filteredJob(crew, "Writing", "department");
+    const cast = filterDuplicates(credits);
 
+    const { media_type } = location.state || {};
+    console.log(media_type)
+    console.log(location.state)
+    console.log(movies)
     useEffect(() => {
-        getMovie(id).then((data) => {
-            setMovie(data.data);
-        });
-        getRecomendMovies(id).then((data) => {
-            setMovies(data.data.results);
-        });
-        getCredits(id).then((data) => {
-            setCredits(data.data.cast);
-            setCrew(data.data.crew);
-        });
-        getMovieTrailer(id).then((data) => {
-            setVideo(data.data.results);
-        });
-    }, [id]);
+        if (media_type && media_type === "tv") {
+            getSeries(id).then((data) => {
+                // console.log(data.data)
+                data && setMovie(data.data);
+            });
+            getSeriesCredits(id).then((data) => {
+                data && setCredits(data.data.cast);
+                data && setCrew(data.data.crew);
+            });
+            getSeriesTrailer(id).then((data) => {
+                data && setVideo(data.data.results);
+            });
+            getRecommendSeries(id).then((data) => {
+                data && setMovies(data.data.results);
+            });
+            getExternalIds(id).then((data) => {
+                data && setExternalIds(data.data);
+            });
+        } else {
+            getMovie(id).then((data) => {
+                console.log(data.data);
+                data && setMovie(data.data);
+            });
+            getRecommendMovies(id).then((data) => {
+                data && setMovies(data.data.results);
+            });
+            getCredits(id).then((data) => {
+                data && setCredits(data.data.cast);
+                data && setCrew(data.data.crew);
+            });
+            getMovieTrailer(id).then((data) => {
+                data && setVideo(data.data.results);
+            });
+        }
+    }, [id, media_type]);
+
+    const movieStatus = [
+        { Canceled: "Отменен" },
+        { Ended: "Завершен" },
+        { Released: "Выпущен" },
+        { ReturningSeries: "Выходит" },
+        { In_Production: "Скоро" },
+    ];
+    
 
     useEffect(() => {
         const trailers = video.filter((item) => item.type === "Trailer");
         setTrailerVideo(trailers);
     }, [video]);
-
     return (
         <>
             <div className="flex flex-col">
@@ -70,9 +118,13 @@ const MovieInformation = () => {
                 </div>
                 <div className="flex sm:flex-row flex-col sm:items-start items-center justify-between xl:px-6">
                     <div className="relative sfhd:w-[650px] sfhd:h-[920px] xl:w-[450px] xl:h-[720px] sm:w-[350px] sm:h-[550px] w-[260px] h-[380px] sm:mb-0 sm:mr-8 mb-4 ring-1 ring-secondary rounded-xl shadow-secondary shadow-[0_0_15px_-3px_var(--tw-shadow-color)]">
-                        {movie.poster_path && (
+                        {movie && (
                             <Poster
-                                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                                src={`https://image.tmdb.org/t/p/original${
+                                    movie.poster_path
+                                        ? movie.poster_path
+                                        : movie.backdrop_path
+                                }`}
                                 alt={""}
                                 className={"rounded-xl"}
                             />
@@ -83,14 +135,37 @@ const MovieInformation = () => {
                         <div>
                             <div className="flex flex-col items-center text-center ss:mb-10 mb-2">
                                 <h1 className="xl:text-5xl text-3xl mb-3">
-                                    {movie.title}
-                                    {movie.release_date
-                                        ? `(${movie.release_date.slice(0, 4)})`
-                                        : ""}
+                                    {media_type === "tv"
+                                        ? movie.name
+                                        : movie.title}
+                                    
+                                    {movie.release_date && (
+                                        `(${movie.release_date.slice(0,4)})`
+                                    )}
                                 </h1>
+                                <p>
+                                    {media_type === "tv" && movie.in_production
+                                        ? `(${movie.first_air_date.slice(0,4)}-...)`: ""}
+                                    {movie.first_air_date &&
+                                        movie.last_air_date && (
+                                            <>
+                                                {!movie.in_production
+                                                    ? `(${movie.first_air_date.slice( 0,4)}
+                                                    -${movie.last_air_date.slice(0,4)})`
+                                                    : ""}
+                                            </>
+                                        )}
+                                </p>
                                 {movie.tagline ? (
                                     <p className="xl:text-3xl text-base">
                                         {movie.tagline}
+                                    </p>
+                                ) : (
+                                    ""
+                                )}
+                                {movie.number_of_seasons ? (
+                                    <p className="xl:text-3xl text-base capitalize">
+                                        {seasonCount}
                                     </p>
                                 ) : (
                                     ""
@@ -147,38 +222,68 @@ const MovieInformation = () => {
                         </div>
                         <div className="flex sm:justify-start justify-center">
                             <ImdbBtn
-                                src={`https://www.imdb.com/title/${movie.imdb_id}`}
+                                src={
+                                    media_type === "tv"
+                                        ? `https://www.imdb.com/title/${externalIds.imdb_id}`
+                                        : `https://www.imdb.com/title/${movie.imdb_id}`
+                                }
                                 className={`mr-3`}
                             />
                             {trailerVideo && (
                                 <TrailerBtn trailer={trailerVideo} />
                             )}
                         </div>
-                        {credits.length && credits.length <= 3 ? (
+                        {cast.length && cast.length <= 3 ? (
                             <>
-                                <h1 className="ss:mt-10 mt-3 mb-3 text-[2.5rem] leading-[2.8rem] text-secondary">Актеры</h1>
-                            <div className="flex   space-x-4 mt-6">
-                                {credits.map((cast) => (
-                                    <CastCard
-                                        key={cast.id}
-                                        cast={cast}
-                                        className=""
-                                    />
-                                ))}
-                            </div>
-                            
+                                <h1 className="ss:mt-10 mt-3 mb-3 text-[2.5rem] leading-[2.8rem] text-secondary">
+                                    Актеры
+                                </h1>
+                                <div className="flex   space-x-4 mt-6">
+                                    {cast.map((actor) => (
+                                        <CastCard
+                                            key={actor.id}
+                                            cast={actor}
+                                            className=""
+                                        />
+                                    ))}
+                                </div>
                             </>
                         ) : (
                             ""
                         )}
                     </div>
                 </div>
-                {credits.length && credits.length > 3 ? (
+                {movie.seasons && (
+                    <div className="mt-8">
+                        <h2 className="text-[2.5rem] leading-[2.8rem] text-secondary">
+                            Сезоны
+                        </h2>
+                        <div className="flex items-start flex-wrap gap-5">
+                            {movie.seasons.map((item) => {
+                                return item.name === "Спецматериалы" ? (
+                                    ""
+                                ) : (
+                                    <div key={item.id} className="mt-3 w-20">
+                                        <div className="">
+                                            <img
+                                                src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+                                                alt="season_poster"
+                                                className="h-full w-full object-contain"
+                                            />
+                                        </div>
+                                        <h2>{item.name}</h2>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+                {cast.length && cast.length > 3 ? (
                     <div className="flex flex-col flex-1.5 ss:mt-10 mt-3">
                         <h1 className="text-[2.5rem] leading-[2.8rem] text-secondary">
                             Актеры
                         </h1>
-                        <ProfileSlider credits={credits} />
+                        <ProfileSlider credits={cast} />
                     </div>
                 ) : (
                     ""
@@ -194,6 +299,16 @@ const MovieInformation = () => {
                                     Информация
                                 </h1>
                                 <ul className="[&_li:not(:last-child)]:mb-3 [&_li]:text-base">
+                                    {movie.number_of_episodes && (
+                                        <li>
+                                            <h2 className=" text-secondary">
+                                                Эпизодов
+                                            </h2>
+                                            <p className=" text-dimWhite">
+                                                {movie.number_of_episodes}
+                                            </p>
+                                        </li>
+                                    )}
                                     {movie.production_countries && (
                                         <li>
                                             <h2 className=" text-secondary">
@@ -211,22 +326,47 @@ const MovieInformation = () => {
                                             )}
                                         </li>
                                     )}
-                                    <li>
-                                        <h2 className="text-secondary">
-                                            Оригинальное название
-                                        </h2>
-                                        <p className="text-dimWhite">
-                                            {movie.original_title}
-                                        </p>
-                                    </li>
-                                    <li>
-                                        <h2 className="text-secondary">
-                                            Премьера в мире
-                                        </h2>
-                                        <p className="text-dimWhite">
-                                            {movie.release_date}
-                                        </p>
-                                    </li>
+                                    {movie.original_title ||
+                                    movie.original_name ? (
+                                        <li>
+                                            <h2 className="text-secondary">
+                                                Оригинальное название
+                                            </h2>
+
+                                            <p className="text-dimWhite">
+                                                {movie.original_title}
+                                                {movie.original_name}
+                                            </p>
+                                        </li>
+                                    ) : (
+                                        ""
+                                    )}
+                                    {movie.release_date ||
+                                    movie.first_air_date ? (
+                                        <li>
+                                            <h2 className="text-secondary">
+                                                Премьера в мире
+                                            </h2>
+                                            <p className="text-dimWhite">
+                                                {movie.release_date}
+                                                {movie.first_air_date}
+                                            </p>
+                                        </li>
+                                    ) : (
+                                        ""
+                                    )}
+                                    {movie.status && (
+                                        <li>
+                                            <h2 className="text-secondary">
+                                                Статус
+                                            </h2>
+                                            <p className="text-dimWhite">
+                                                {movieStatus.map(
+                                                    (item) => item[movie.status]
+                                                )}
+                                            </p>
+                                        </li>
+                                    )}
                                 </ul>
                             </div>
                             <div className="flex-1">
