bcff0677ddae42ecc8a35094ff6138d9560df63c
Vlad <sambipush@mail.ru>
2024-02-20 11:38:01 +0300

    Add state homePage to Link, so that when you click on the logo, the page becomes 1.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { logo } from "../assets";
 const Navbar = () => (
     <nav className="flex ss:flex-1 relative items-center ss:justify-between justify-start w-full">
         <div className="flex flex-1">
-            <Link to={"/"}>
+            <Link to={"/"} state={{homePage: 1}}>
                 <img src={logo} alt="logo" className="sfhd:w-[250px] sfhd:h-[60px] w-[150px] h-[30px]" />
             </Link>
         </div>
