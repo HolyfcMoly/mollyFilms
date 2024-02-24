c500f2b9f263539e8480539f82f3689c887adde4
Vlad <sambipush@mail.ru>
2024-02-24 11:00:19 +0300

    create max page constant

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,6 @@
 const API_KEY = import.meta.env.VITE_MOVIE_API_KEY
+const MAX_PAGES = 500
 
 const API_URL = "https://api.themoviedb.org/3/"
 
-export {API_URL, API_KEY}
+export {API_URL, API_KEY, MAX_PAGES}
