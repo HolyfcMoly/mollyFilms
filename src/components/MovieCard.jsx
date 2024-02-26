ed00fefbbde11864d3313fdacd5e42409fe75fac
Vlad <sambipush@mail.ru>
2024-02-26 12:10:36 +0300

    added serial img, media type

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ import Poster from "./ui/Poster";
 import { Rating, ThinStar } from "@smastrom/react-rating";
 import { useInView } from "react-intersection-observer";
 import ImgPreLoader from "./ui/ImgPreLoader";
-
+import { serial } from "../assets";
 
 const myStyles = {
     itemShapes: ThinStar,
@@ -17,10 +17,12 @@ const MovieCard = ({ movieGenre, movie, containerClass = ''}) => {
         threshold: 0.2,
         triggerOnce: true,
     });
+
     return (
         <div className={`p-2 sfhd:w-[350px] ss:w-[200px] xss:w-[150px] w-[200px]  max-h-[100%] rounded-xl ${containerClass}`} ref={ref}>
             <Link
                 to={`/movie/${movie.id}`}
+                state={{media_type: movie.media_type ? movie.media_type : movie.first_air_date ? 'tv' : 'movie'}}
                 className="flex flex-col relative group focus:outline-none"
             >
                 <div
@@ -29,16 +31,20 @@ const MovieCard = ({ movieGenre, movie, containerClass = ''}) => {
                 >
                     {inView ? (
                             <Poster
-                                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                                src={movie.media_type === 'person' ? `https://image.tmdb.org/t/p/original${movie.profile_path}` : `https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                                
                                 alt={movie.title}
                                 className={`sfhd:rounded-xl rounded-md bg-transparent`}
                             />
                     ) : (
                         <ImgPreLoader />
                     )}
+                    {movie.media_type === 'tv' || movie.first_air_date ? (
+                        <img src={serial} alt="serial" className="w-6 h-6 absolute top-1 right-1 group-hover:z-50"/>
+                    ) : ''}
                     <div className="flex flex-col justify-end opacity-0 transition-all rounded-xl group-focus:opacity-100 hover:opacity-100 hover:shadow-[inset_0_-100px_50px_-55px_rgba(0,0,0,1)] group-focus:shadow-[inset_0_-100px_50px_-55px_rgba(0,0,0,1)] absolute inset-0 bg-neutral-900 bg-opacity-40 h-full sfhd:px-5 px-2 py-5">
                         <div className={`flex`}>
-                            <p className="sfhd:text-2xl">{movie.vote_average.toFixed(1)}</p>
+                            <p className="sfhd:text-2xl">{movie.vote_average ? movie.vote_average.toFixed(1) : movie.vote_average}</p>
                             <Rating
                                 readOnly
                                 value={movie.vote_average / 2}
@@ -50,6 +56,9 @@ const MovieCard = ({ movieGenre, movie, containerClass = ''}) => {
                             {movie.release_date
                                 ? movie.release_date.slice(0, 4)
                                 : movie.release_date}
+                            {movie.first_air_date
+                                ? movie.first_air_date.slice(0, 4)
+                                : movie.first_air_date}
                         </h3>
                         {movieGenre && movieGenre.map((genre) =>
                             movie.genre_ids && genre.id === movie.genre_ids[0] ? (
@@ -65,7 +74,11 @@ const MovieCard = ({ movieGenre, movie, containerClass = ''}) => {
                         )}
                     </div>
                 </div>
-                <h3 className="sfhd:text-3xl">{movie.title}</h3>
+                <h3 className="sfhd:text-3xl">
+                    {movie.media_type === "tv" || movie.first_air_date
+                        ? movie.name
+                        : movie.title}
+                </h3>
             </Link>
         </div>
     );
