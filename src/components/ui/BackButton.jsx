2187bc481be8b27a3509e343241a91d47bc9aa7f
Vlad <sambipush@mail.ru>
2024-03-12 10:52:22 +0300

    Added second parameter to call multiply functions

--- src/components/ui/BackButton.jsx
+++ src/components/ui/BackButton.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import IconArrowForward from "../../assets/icons/IconArrowForward";
 
-const BackButton = ({className}) => {
+const BackButton = ({className, actions}) => {
     const navigate = useNavigate();
+    const handleClick = () => {
+        navigate(-1);
+        actions?.forEach(action => action())
+    }
+    
     return (
         <button
             className={`block pr-4 pl-2 py-2 bg-transparent text-secondary group hover:bg-secondary hover:text-white rounded-xl ring-1 ring-secondary transition-colors sfhd:text-2xl xl:text-lg text-base ${className} `}
-            onClick={() => navigate(-1)}
+            onClick={handleClick}
         >
             {<IconArrowForward className={`sfhd:w-8 sfhd:h-8 xl:w-6 xl:h-6 w-5 h-5 scale-[-1] fill-secondary transition-colors group-hover:fill-white`}/>}
             Назад
