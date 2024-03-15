366cf92442a5dee6bdbc3dc9478268cd787f9c42
Vlad <sambipush@mail.ru>
2024-03-15 12:58:54 +0300

    Fixed history navigation. Use useSearchParams hook to get parametrs from url

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -2,112 +2,81 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import {
     refreshAPI,
-    getMovieByQuerySearch,
     getMovies,
     getMoviesByGenre,
     getPopularTv,
+    getMultiSearchByQuery,
 } from "../services/api";
 import MovieList from "../components/MovieList";
-import { useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import Pagination from "../components/Pagination";
 import Preloader from "../components/ui/Preloader";
+import { excludeGenres } from "../assets/icons/genres";
+import { MAX_PAGES } from "../services/config";
 
 const Movies = () => {
     const [genreMovie, setGenreMovie] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const defaultPage = localStorage.getItem('page') || 1 ;
-    const [currentPage, setCurrentPage] = useState(defaultPage);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const currentPage = searchParams.get('p') || '1';
+    const movieId = isNaN(searchParams.get('id')) ? searchParams.get('id') : Number(searchParams.get('id'));
+    const query = searchParams.get('q') || '';
+    const genre = searchParams.get('g') || '';
+    const search = searchParams.get('s') || false;
+
     const [pages, setPages] = useState(0);
-    const [movieTotalPages, setMovieTotalPages] = useState(1);
-    const [tvsTotalPages, setTvsTotalPages] = useState(1);
-    const [prevGenre, setPrevGenre] = useState('');
-    
-    localStorage.setItem('page', currentPage);
-    const storage = localStorage.getItem('page');
-    
-    const location = useLocation();
-    const { movies, query, genre } = !location.state
-    ? genreMovie
-    : location.state;
-
-    useEffect(() => {
-        const isChanged = localStorage.getItem('isGenreChanged');
-        if(genre === prevGenre) {
-            setCurrentPage(storage);
-            if(isChanged) {
-                setCurrentPage(1)
-                localStorage.removeItem('isGenreChanged')
-            }
-        }
-
-        if(genre !== prevGenre) {
-            setPrevGenre(genre)
-            if(isChanged) {
-                setCurrentPage(1)
-                localStorage.removeItem('isGenreChanged')
-            }
-        } 
-
-        if(!movies && !location.state?.homePage) {
-            setCurrentPage(storage)
-        }
-
-        if(location.state?.homePage) {
-            setCurrentPage(1);
-            location.state.homePage = null;
-        }
-    }, [location.state, storage, genre, prevGenre, movies]);
-
 
     useEffect(() => {
         if (!query) {
-            if (!movies) {
-                console.log(movies)
+            if (!movieId) {
                 refreshAPI();
                 Promise.all([
-                    getMovies(movies, currentPage),
+                    getMovies(movieId, currentPage),
                     getPopularTv(currentPage),
                 ]).then(([movie, tv]) => {
-                    const allGenres = [
-                        ...movie.data.results,
-                        ...tv.data.results,
-                    ].sort((a, b) => b.popularity - a.popularity);
-                    setGenreMovie(allGenres);
-                    const moviesTotalPages = movie.data.total_pages;
-                    const tvTotalPages = tv.data.total_pages;
+                    if(movie?.data?.results && tv?.data?.results) {
+                        const allGenres = [
+                            ...movie.data.results,
+                            ...tv.data.results,
+                        ]
+                        .filter(genre => {
+                            return !genre.genre_ids.some(id => excludeGenres.includes(id))
+                        })
+                        .sort((a, b) => b.vote_average - a.vote_average)
+                        setGenreMovie(allGenres);
+                    }
+                    const moviesTotalPages = movie?.data?.total_pages;
+                    const tvTotalPages = tv?.data?.total_pages;
                     const totalPages = Math.min(moviesTotalPages, tvTotalPages);
-                    if (totalPages >= 500) {
-                        setPages(500);
+                    if (totalPages >= MAX_PAGES) {
+                        setPages(MAX_PAGES);
                     } else {
                         setPages(totalPages);
                     }
                 });
             }
-            if (movies && typeof movies === "string") {
+            if (movieId && typeof movieId === "string") {
                 refreshAPI();
-                getMovies(movies, currentPage).then((data) => {
+                getMovies(movieId, currentPage).then((data) => {
                     data?.data?.results && setGenreMovie(data.data.results);
-                    data?.data?.page && setCurrentPage(data.data.page);
                     data?.data?.total_pages && setPages(data.data.total_pages);
                 });
             }
-            if (movies && typeof movies === "number") {
+            if (movieId && typeof movieId === "number") {
                 refreshAPI();
-                getMoviesByGenre(movies, currentPage).then((data) => {
+                getMoviesByGenre(movieId, currentPage).then((data) => {
                     data?.data?.results && setGenreMovie(data.data.results);
-                    data?.data?.page && setCurrentPage(data.data.page);
                     data?.data?.total_pages && setPages(data.data.total_pages);
                 });
             }
         } else {
             refreshAPI();
-            getMovieByQuerySearch(query, currentPage).then((data) => {
+            getMultiSearchByQuery(query, currentPage).then((data) => {
                 data?.data?.results && setGenreMovie(data.data.results);
-                data?.data?.page && setCurrentPage(data.data.page);
                 data?.data?.total_pages && setPages(data.data.total_pages);
             });
         }
-    }, [query, movies, currentPage, storage]);
+    }, [query, movieId, currentPage]);
 
     return (
         <>
@@ -119,13 +88,16 @@ const Movies = () => {
                         <MovieList
                             movie={genreMovie}
                             genre={genre}
+                            query={query}
+                            search={search}
                         />
                     </div>
-                    <Pagination
-                        currentPage={currentPage}
-                        setPage={setCurrentPage}
-                        totalPages={pages}
-                    />
+                        <Pagination
+                            currentPage={currentPage}
+                            searchParams={searchParams}
+                            setPage={setSearchParams}
+                            totalPages={pages}
+                        />
                 </>
             )}
         </>
