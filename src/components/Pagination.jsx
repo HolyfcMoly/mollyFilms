73ca50c22e3e4968965707a45f08dbacc82b24d0
Vlad <sambipush@mail.ru>
2024-03-21 11:28:52 +0300

    Update Pagination component. Added package react-paginate. Create ref to save current page and after refresh page or open in a new tab the page is not lost. And also created some states for other params from the  url, so that when changing some of the parameters the page will be reset

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,47 +1,58 @@
-import React from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import ReactPaginate from 'react-paginate'
+import { MAX_PAGES } from "../services/config";
 
-const Pagination = ({ currentPage, searchParams, setPage, totalPages }) => {
-    const newParams = new URLSearchParams(searchParams); 
-
-    const setUrlQuery = (urlParams, btn) => {
-        if(btn === 'prev' || btn === 'next') {
-            urlParams.set('p', `${+currentPage + (btn === 'prev' ? - 1 : + 1)}`);
+const Pagination2 = ({ searchParams, setPage, totalPages, profile = false }) => {
+    const newParams = useMemo(() => new URLSearchParams(searchParams), [searchParams]); 
+    const [prevId, setPrevId] = useState(null);
+    const [prevGenre, setPrevGenre] = useState(null);
+    const [prevQuery, setPrevQuery] = useState(null);
+    const id = newParams.get('id');
+    const g = newParams.get('g');
+    const q = newParams.get('q');
+    const ref = useRef(1)
+    
+    const setUrlQuery = (urlParams, page) => {
+        if(page) {
+            urlParams.set('p', page.toString())
             setPage(urlParams.toString())
-        }
-        if(btn === 'reset') {
-            urlParams.set('p', '1');
-            setPage(urlParams)
         }
     }
 
-    const handlePrev = () => {
-        if (+currentPage !== 1) {
-            setUrlQuery(newParams, 'prev');
+    useEffect(() => {
+        if(id !== prevId || g !== prevGenre || q !== prevQuery) {
+            setPrevId(id);
+            setPrevGenre(g);
+            setPrevQuery(q);
         }
-    };
-    const handleNext = () => {
-        if (+currentPage < totalPages) {
-            setUrlQuery(newParams, 'next');
-        } 
-    };
-    const handleReset = () => {
-        if (+currentPage !== 1) {
-            setUrlQuery(newParams, 'reset');
-        }
-    };
+    },[newParams, prevId, prevGenre, g, id, q, prevQuery])
+
+    useEffect(() => {
+        ref.current = +newParams.get('p') || 1;
+    }, [newParams])
+
+    const handlePageClick = (event) => {
+        ref.current = event.selected +1
+        setUrlQuery(newParams, event.selected + 1)
+    }
 
     return (
         <div className="flexCenter py-6">
-            <button onClick={handleReset}>В начало</button>
-            <button className="mx-8" onClick={handlePrev}>
-                Prev
-            </button>
-            <span>{currentPage}</span>
-            <button className="mx-8" onClick={handleNext}>
-                Next
-            </button>
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                marginPagesDisplayed={1}
+                onPageChange={handlePageClick}
+                forcePage={profile ? 0 : ref.current - 1}
+                pageRangeDisplayed={1}
+                pageCount={totalPages > MAX_PAGES ? MAX_PAGES : totalPages}
+                previousLabel="< previous"
+                renderOnZeroPageCount={null}
+                containerClassName="flex gap-6"
+            />
         </div>
     );
 };
 
-export default Pagination;
+export default Pagination2;
+
