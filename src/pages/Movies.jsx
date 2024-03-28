954628ba90d9fd201b8080a2d6f0e7cc6adb7ac0
Vlad <sambipush@mail.ru>
2024-03-28 14:02:48 +0300

    Update useEffect logic. Added new if conditions to check type and call correct function

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -4,8 +4,9 @@ import {
     refreshAPI,
     getMovies,
     getMoviesByGenre,
-    getPopularTv,
+    getCategoryTv,
     getMultiSearchByQuery,
+    getTvByGenre,
 } from "../services/api";
 import MovieList from "../components/MovieList";
 import { useSearchParams } from "react-router-dom";
@@ -17,22 +18,25 @@ import { MAX_PAGES } from "../services/config";
 const Movies = () => {
     const [genreMovie, setGenreMovie] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [allMovies, setAllMovies] = useState(false);
     const [searchParams, setSearchParams] = useSearchParams();
     const currentPage = searchParams.get('p') || '1';
     const movieId = isNaN(searchParams.get('id')) ? searchParams.get('id') : Number(searchParams.get('id'));
     const query = searchParams.get('q') || '';
     const genre = searchParams.get('g') || '';
+    const type = searchParams.get('t') || '';
     const search = searchParams.get('s') || false;
-
     const [pages, setPages] = useState(0);
+    const typeStorage = localStorage.getItem('type');
 
     useEffect(() => {
+        setAllMovies(false);
         if (!query) {
             if (!movieId) {
                 refreshAPI();
                 Promise.all([
                     getMovies(movieId, currentPage),
-                    getPopularTv(currentPage),
+                    getCategoryTv('',currentPage),
                 ]).then(([movie, tv]) => {
                     if(movie?.data?.results && tv?.data?.results) {
                         const allGenres = [
@@ -44,6 +48,7 @@ const Movies = () => {
                         })
                         .sort((a, b) => b.vote_average - a.vote_average)
                         setGenreMovie(allGenres);
+                        setAllMovies(true);
                     }
                     const moviesTotalPages = movie?.data?.total_pages;
                     const tvTotalPages = tv?.data?.total_pages;
@@ -55,14 +60,54 @@ const Movies = () => {
                     }
                 });
             }
-            if (movieId && typeof movieId === "string") {
+            if(type && type === 'movie' && !genre) {
+                refreshAPI();
+                getMovies("popular", currentPage).then(data => {
+                    data?.data?.results && setGenreMovie(data.data.results);
+                    data?.data?.total_pages && setPages(data.data.total_pages);
+                })
+            } 
+            if(type && type === 'tv' || typeStorage && typeStorage === 'tv') {
+                if(!movieId) {
+                    refreshAPI();
+                    getCategoryTv('',currentPage).then(data => {
+                        if(data?.data?.results) {
+                            const series = data.data.results.filter(genre => {
+                                return !genre.genre_ids.some(id => excludeGenres.includes(id))
+                            })
+                            setGenreMovie(series)
+                        }
+                        data?.data?.total_pages && setPages(data.data.total_pages);
+                    })
+                }
+                if(movieId && typeof movieId === "string") {
+                    refreshAPI();
+                    getCategoryTv(movieId, currentPage).then(data => {
+                        if(data?.data?.results) {
+                            const series = data.data.results.filter(genre => {
+                                return !genre.genre_ids.some(id => excludeGenres.includes(id))
+                            })
+                            setGenreMovie(series)
+                        }
+                        data?.data?.total_pages && setPages(data.data.total_pages);
+                    })
+                }
+                if(movieId && typeof movieId === 'number') {
+                    refreshAPI();
+                    getTvByGenre(movieId, currentPage).then((data) => {
+                        data?.data?.results && setGenreMovie(data.data.results);
+                        data?.data?.total_pages && setPages(data.data.total_pages);
+                    });
+                }
+            }
+            if (movieId && typeof movieId === "string" && !type && typeStorage === 'movie') {
                 refreshAPI();
                 getMovies(movieId, currentPage).then((data) => {
                     data?.data?.results && setGenreMovie(data.data.results);
                     data?.data?.total_pages && setPages(data.data.total_pages);
                 });
             }
-            if (movieId && typeof movieId === "number") {
+            if (movieId && typeof movieId === "number" && !type && typeStorage === 'movie') {
                 refreshAPI();
                 getMoviesByGenre(movieId, currentPage).then((data) => {
                     data?.data?.results && setGenreMovie(data.data.results);
@@ -76,7 +121,7 @@ const Movies = () => {
                 data?.data?.total_pages && setPages(data.data.total_pages);
             });
         }
-    }, [query, movieId, currentPage]);
+    }, [query, movieId, currentPage, type, genre, typeStorage]);
 
     return (
         <>
@@ -90,10 +135,10 @@ const Movies = () => {
                             genre={genre}
                             query={query}
                             search={search}
+                            all={allMovies}
                         />
                     </div>
                         <Pagination
-                            currentPage={currentPage}
                             searchParams={searchParams}
                             setPage={setSearchParams}
                             totalPages={pages}
