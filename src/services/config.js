c23c3b610c3d4479843311bdbdb2d1a5ddf257c5
Vlad <sambipush@mail.ru>
2024-03-28 13:19:20 +0300

    Added new key variable

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,7 @@
 const API_KEY = import.meta.env.VITE_MOVIE_API_KEY
 const MAX_PAGES = 500
+const YANDEX_API_KEY = import.meta.env.VITE_YANDEX_TRANSLATE_API_KEY
 
 const API_URL = "https://api.themoviedb.org/3/"
 
-export {API_URL, API_KEY, MAX_PAGES}
+export {API_URL, API_KEY, MAX_PAGES, YANDEX_API_KEY}
