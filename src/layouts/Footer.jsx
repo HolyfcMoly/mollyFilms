9c8ea4ba1845706c718d13a66fbc43a748fb1276
Vlad <sambipush@mail.ru>
2024-03-29 21:49:53 +0300

    Added link to repo

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { logo } from "../assets";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        const newParams = new URLSearchParams({h: 't'})
+        localStorage.setItem('type', 'movie');
+        navigate(`/?${newParams.toString()}`)
+    }
+
     return (
         <div className="flex items-center justify-between mt-10 py-3 relative before:absolute before:top-0 before:left-0 before:w-full before:bg-dark before:h-[1px]">
-            <Link to={"/"}>
-                <img
-                    src={logo}
-                    alt="logo"
-                    className="sfhd:w-[250px] sfhd:h-[60px] w-[150px] h-[30px]"
-                />
-            </Link>
-            <a href="!#">CodeLink</a>
+            <div onClick={handleClick} className="cursor-pointer">
+                <img src={logo} alt="logo" className="sfhd:w-[250px] sfhd:h-[60px] w-[150px] h-[30px]" />
+            </div>
+            <a href="https://github.com/HolyfcMoly/mollyFilms" rel="noreferrer" target="_blank">CodeLink</a>
         </div>
     );
 };
